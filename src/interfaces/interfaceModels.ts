159c80624b7cfb172415d297fa180bfc7be26b52
Edgar Chavero <edieveg316@gmail.com>
2022-07-20 16:33:09 -0500

    finish interface

--- src/interfaces/interfaceModels.ts
+++ src/interfaces/interfaceModels.ts
@@ -1,3 +1,5 @@
+import { Types } from "mongoose";
+
 export interface IUser {
   _id?: string;
   username: string;
@@ -12,7 +14,7 @@ export interface IUser {
 export interface Business {
   _id?: string;
   name: string;
-  author: string;
+  author: Types.ObjectId;
   address: string;
   image: string;
   contact: [
@@ -25,7 +27,7 @@ export interface Business {
   ];
   budget: number;
   typeBusiness: string;
-  reviews: [Array: unknown];
+  reviews: [Review: unknown];
   rating: number;
   numReviews: number;
   timestamps: {
@@ -35,7 +37,7 @@ export interface Business {
 }
 
 export interface Doctor {
-  _id: string;
+  _id?: string;
   name: string;
   author: string;
   address: string;
@@ -49,7 +51,7 @@ export interface Doctor {
       instagram?: string;
     }
   ];
-  reviews: [Array: unknown];
+  reviews: [Review: unknown];
   rating: number;
   numReviews: number;
   timestamps: {
@@ -59,13 +61,158 @@ export interface Doctor {
 }
 
 export interface Market {
-  _id: string;
+  _id?: string;
   name: string;
   author: string;
   address: string;
   image: string;
   typeMarket: string;
-  reviews: [Array: unknown];
+  reviews: [Review: unknown];
+  rating: number;
+  numReviews: number;
+  timestamps: {
+    createdAt: Date;
+    updatedAt: Date;
+  };
+}
+
+export interface Post {
+  _id?: string;
+  user: string;
+  text: string;
+  name: string;
+  avatar: string;
+  likes: [{ user: Types.ObjectId }];
+  comments: [
+    {
+      user: Types.ObjectId;
+      text: string;
+      name: string;
+      avatar: string;
+      date: Date;
+    }
+  ];
+  date: Date;
+  timestamps: {
+    createdAt: Date;
+    updatedAt: Date;
+  };
+}
+
+export interface Profession {
+  _id?: string;
+  professionName: string;
+  author: Types.ObjectId;
+  specialty: string;
+  contact: [
+    {
+      phone: number;
+      email: string;
+      facebook?: string;
+      instagram?: string;
+    }
+  ];
+  reviews: [Review: unknown];
+  rating: number;
+  numReviews: number;
+  timestamps: {
+    createdAt: Date;
+    updatedAt: Date;
+  };
+}
+
+export interface Review {
+  _id?: string;
+  username: string;
+  rating: number;
+  comment: string;
+  user: Types.ObjectId;
+  timestamps: {
+    createdAt: Date;
+    updatedAt: Date;
+  };
+}
+
+export interface ProfessionProfile {
+  _id?: string;
+  user: Types.ObjectId;
+  contact: [{ phone: number; email: string }];
+  skills: [string];
+  experience: [
+    {
+      title: string;
+      company: string;
+      location: string;
+      from: Date;
+      to: Date;
+      current: boolean;
+      description: string;
+    }
+  ];
+  education: [
+    {
+      school: string;
+      degree: string;
+      fieldOfStudy: string;
+      from: Date;
+      to: Date;
+      current: boolean;
+      description: string;
+    }
+  ];
+  social: [
+    {
+      youtube: string;
+      facebook: string;
+      twitter: string;
+      instagram: string;
+      linkedin: string;
+    }
+  ];
+  date: Date;
+  timestamps: {
+    createdAt: Date;
+    updatedAt: Date;
+  };
+}
+
+export interface Recipe {
+  _id?: string;
+  title: string;
+  author: Types.ObjectId;
+  description: string;
+  instructions: string;
+  ingredients: [string];
+  typeDish: string;
+  image: string;
+  cookingTime: number;
+  difficulty: string;
+  reviews: [Review: unknown];
+  rating: number;
+  numReviews: number;
+  budget: string;
+  timestamps: {
+    createdAt: Date;
+    updatedAt: Date;
+  };
+}
+
+export interface Restaurant {
+  _id?: string;
+  name: string;
+  author: Types.ObjectId;
+  typePlace: string;
+  address: string;
+  image: string;
+  budget: string;
+  contact: [
+    {
+      phone: number;
+      facebook: string;
+      instagram: string;
+    }
+  ];
+  reviews: [Review: unknown];
   rating: number;
   numReviews: number;
   timestamps: {
--- src/models/User.ts
+++ src/models/User.ts
@@ -2,10 +2,64 @@ import bcrypt from "bcryptjs";
 import mongoose from "mongoose";
 const { Schema } = mongoose;
 
+import { IUser } from "../interfaces/interfaceModels";
 
-
-const userSchema = new Schema<IUser>({
-  _id: {
-    type: String,
+const userSchema = new Schema<IUser>(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    role: {
+      type: String,
+      required: true,
+      enum: ["user", "admin", "professional"],
+      default: "user",
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    photo: {
+      type: String,
+      // eslint-disable-next-line prettier/prettier
+      default: "https://res.cloudinary.com/dzqbzqgjm/image/upload/v1599098981/default-user_qjqjqz.png",
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    isAdmin: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+    isProfessional: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
   },
+  { timestamps: true }
+);
+
+// check if password is correct
+
+// eslint-disable-next-line prettier/prettier
+userSchema.methods.isCorrectPassword = async function (candidatePassword: string) {
+  return await bcrypt.compare(candidatePassword, this.password);
+};
+
+// hash the password before saving
+
+userSchema.pre("save", async function (next) {
+  if (this.isModified("password")) {
+    this.password = await bcrypt.hash(this.password, 10);
+  }
+  next();
 });
+
+const User = mongoose.model<IUser>("User", userSchema);
+export default User;
