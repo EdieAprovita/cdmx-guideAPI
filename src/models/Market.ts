62504e825220a4283a0367e91f2884e0b3f7654b
Edgar Chavero <edieveg316@gmail.com>
2022-11-14 21:47:52 -0600

    Create Market.ts

---
+++ src/models/Market.ts
@@ -0,0 +1,49 @@
+import mongoose from "mongoose";
+const { Schema } = mongoose;
+
+import { IMarket } from "src/interfaces/interfaceModels";
+
+const marketSchema = new Schema<IMarket>(
+    {
+        marketName: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        author: {
+            type: Schema.Types.ObjectId,
+            ref: "User",
+        },
+        address: {
+            type: String,
+            required: true,
+        },
+        image: {
+            type: String,
+            required: true,
+        },
+        typeMarket: {
+            type: String,
+            required: true,
+            enum: ["supermarket", "convenience store", "grocery store"],
+        },
+        reviews: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: "Review",
+            },
+        ],
+        rating: {
+            type: Number,
+            required: true,
+        },
+        numReviews: {
+            type: Number,
+            required: true,
+        },
+    },
+    { timestamps: true }
+);
+
+const Market = mongoose.model<IMarket>("Market", marketSchema);
+export default Market;
